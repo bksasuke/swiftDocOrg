1527135f9c5b799946b7480a26d3527b28668eec
Ta Quang Tuan <tuantq16@fsoft.com.vn>
2020-01-16 10:04:09 +0700

    Initial Commit

---
+++ SwiftProgramingLang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftProgramingLang
+//
+//  Created by Tuan on 1/16/20.
+//  Copyright © 2020 Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
